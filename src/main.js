bbe44be6f99df758731d172ca583dea5ab84abef
周纤纤 <xianxian@51jianjiao.com>
2019-08-19 19:12:29 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 import Mention from './pages/mention.vue'
 export default Mention;
 
 // Vue.config.productionTip = false
 
-// new Vue({
-//   render: h => h(App),
-// }).$mount('#app')
+new Vue({
+    render: h => h(Mention),
+}).$mount('#app')
